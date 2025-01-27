5801c983e0047c4801ff5ba9b0b4dcb49c447faa
petavlis <canalcalmaalma@gmail.com>
2025-01-27 14:54:39 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Olá, Flask está funcionando!"
+    return render_template("index.html")  # Certifique-se de que 'index.html' está na pasta 'templates'
 
 if __name__ == "__main__":
     app.run(debug=True)
